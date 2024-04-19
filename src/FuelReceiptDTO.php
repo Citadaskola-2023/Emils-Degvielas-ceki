89380d0b3e2103a47a95b926be1770244a9c0a34
Emīls Berķis <emils.berkis@citadaskola.lv>
2024-04-19 14:30:30 +0300

    Degviela

---
+++ src/FuelReceiptDTO.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App;
+
+class FuelReceiptDTO
+{
+    public function __construct(
+        public string $licencePlate,
+        public string $dateTime,
+        public string $odometer,
+        public string $petrolStation,
+        public string $fuelType,
+        public string $refueled,
+        public string $currency,
+        public string $fuelPrice,
+    )
+    {
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'licencePlate' => $this->licencePlate,
+            'dateTime' => $this->dateTime,
+            'odometer' => $this->odometer,
+            'petrolStation' => $this->petrolStation,
+            'fuelType' => $this->fuelType,
+            'refueled' => $this->refueled,
+            'total' => 69,
+            'currency' => $this->currency,
+            'fuelPrice' => $this->fuelPrice,
+        ];
+    }
+
+}
